2e97a477fd634f4d29edb9316c7c3ece785a3280
Chigusa <chigusa.unity@gmail.com>
2018-06-14 16:26:03 +0900

    Step

--- Assets/DammyScript.cs
+++ Assets/DammyScript.cs
@@ -8,7 +8,7 @@ public class DammyScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        Debug.Log(this.name);
     }
 
     // Update is called once per frame
