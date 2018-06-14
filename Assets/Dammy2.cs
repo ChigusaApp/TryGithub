6684a3cf0979fbb00076d5ea9f93697238ec5791
Chigusa <chigusa.unity@gmail.com>
2018-06-14 17:23:36 +0900

    edit dammy2

--- Assets/Dammy2.cs
+++ Assets/Dammy2.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Reflection;
 using UnityEngine;
 
 public class Dammy2 : MonoBehaviour
@@ -8,7 +7,7 @@ public class Dammy2 : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        Debug.Log(string.Format("{0} {1} {2}", this.GetType().Name, MethodBase.GetCurrentMethod().Name, this.GetType().FullName));
     }
 
     // Update is called once per frame
